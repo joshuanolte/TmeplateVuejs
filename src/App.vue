60938884b291f8f9e4e04f48ab80fdc2a4848c35
NolteJoshua <81680904+joshuanolte@users.noreply.github.com>
2023-10-12 12:03:15 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container-fluid">
+    <div class="container-content">
+      <div class="btn-content">
+        <div class="btn-group" >
+          <button type="button" class="btn btn-secondary">Login</button>
+          <button type="button" class="btn btn-secondary">Sign Up</button>
+        </div>
+      </div>
+    </div>
+    <div class="container-content">
+      <div class="header-container">
+        <div class="page-header">
+          <h1>Car Wash</h1>
+          <label class="lblHeader">We wash Car</label>
+        </div>
+      </div>
+    </div>
+    <div class="main-content">
+      <div class="site-content">
+        <div class="side-content">
+          </div>
+
+        <div class="items-content">
+          <div class="card">
+            <div class="card-body">
+              <h1 class="card-title">About Us</h1>
+              <p class="card-text">testerstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesters
+                testerstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesters
+                testerstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstestersvtesterstesterstesterstesters
+                testerstesterstesterstesters</p>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="card-body">
+              <h1 class="card-title">Services</h1>
+              <p class="card-text">testerstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesters
+                testerstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesters
+                testerstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstesterstestersvtesterstesterstesterstesters
+                testerstesterstesterstesters</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+@import './assets/styles/mainlayout.css';
+@import './assets/styles/cards.css';
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+ import App from './App.vue'
+// import LoginOverLayer from "@/components/LoginOverLayer.vue";
+import 'bootstrap/dist/css/bootstrap.css'
+
+ createApp(App).mount('#app')
+// createApp(LoginOverLayer).mount('#app')
